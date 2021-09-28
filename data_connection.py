5f79028a33076f0a106f7867fdb29b127f286a8e
Justin E <justinedwards1230@gmail.com>
2021-09-28 01:36:06 -0400

    Update data_connection.py
    Created var for db type for consistency

--- data_connection.py
+++ data_connection.py
@@ -2,13 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+db_type = 'postgresql'
 db_user = 'usr'
 db_pass = 'pass'
 db_ip = '192.168.1.120'
 db_port = '5432'
 db_name = 'my_fit_timer_db'
 
-engine = create_engine(f'postgresql://{db_user}:{db_pass}@{db_ip}:{db_port}/{db_name}')
+engine = create_engine(f'{db_type}://{db_user}:{db_pass}@{db_ip}:{db_port}/{db_name}')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
