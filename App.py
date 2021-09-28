e3dc1634144edce56f81a6aeb78734d85a17a61a
Justin E <justinedwards1230@gmail.com>
2021-09-28 01:38:06 -0400

    Cleaned and commented

--- App.py
+++ App.py
@@ -9,9 +9,13 @@ class Entry(Base):
     __tablename__ = 'times_table'
 
     # Create columns
-    time_idx = Column(Integer, primary_key=True, autoincrement=True)
-    start_time = Column(BIGINT)
-    stop_time = Column(BIGINT)
+    time_idx = Column(Integer, 
+                        primary_key=True, 
+                        autoincrement=True)
+    start_time = Column(BIGINT, 
+                        nullable=False)
+    stop_time = Column(BIGINT, 
+                        nullable=False)
 
     # Initialize object with times
     def __init__(self, sTime, spTime):
@@ -20,12 +24,15 @@ class Entry(Base):
         # Create table if it does not exist
         engine.execute("CREATE TABLE IF NOT EXISTS times_table (time_idx int, start_time BIGINT, stop_time BIGINT)")
 
+# Create SQL tables
+Base.metadata.create_all(engine)
 
 # Start Server
 app = Flask("Timer")
 app.debug = True
-Base.metadata.create_all(engine)
 
+# Pull historic data from db
+# Returns list of all recorded times
 def grab_history():
     # Create SQL session
     session = Session()
@@ -34,10 +41,12 @@ def grab_history():
     times = session.query(Entry).all()
     session.close()
 
-    # Assign times to array
-    all_times = [{'startTime':time.start_time,'stopTime':time.stop_time} for time in times]
+    # Assign times to list
+    all_times = [{'startTime':time.start_time,
+                    'stopTime':time.stop_time} 
+                    for time in times]
 
-    # Return JSON object of historic times
+    # Return list object of historic times
     return all_times
 
 # Serve Main page
@@ -53,17 +62,20 @@ def index():
 def get_post_javascript_data():
     # Pull JSON from POST
     jsdata = request.get_json()
-    # Create SQL session
-    session = Session()
-    # Assign times to Entry object
-    entry = Entry(jsdata.get('startTime'), jsdata.get('stopTime'))
-    # Add entry object to SQL session
-    session.add(entry)
-    # Delete entry object
-    del entry
-    # Push entry and close SQL session
-    session.commit()
-    session.close()
+    # Verify data comes as positive integers
+    if jsdata.get('startTime') > 0 and jsdata.get('stopTime') > 0:
+        # Create SQL session
+        session = Session()
+        # Assign times to Entry object
+        entry = Entry(jsdata.get('startTime'), 
+                        jsdata.get('stopTime'))
+        # Add entry object to SQL session
+        session.add(entry)
+        # Delete entry object
+        del entry
+        # Push entry and close SQL session
+        session.commit()
+        session.close()
 
     return '/'
 
